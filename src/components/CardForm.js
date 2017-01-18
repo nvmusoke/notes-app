dc695f5506b3bbf37ca65dc6732dc498d97f2645
christopher barragan <barraganchris95@gmail.com>
2017-01-18 12:34:38 -0600

    commited my new CardForm.js

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -15,28 +15,13 @@ class CardForm extends Component {
   render() {
     return (
       <div className="container">
-<<<<<<< HEAD
-
-        <input ref="titleValue" onKeyUp={this.handleTitleKeyUp.bind(this)} type="text" placeholder="title"/>
-        <textarea onKeyUp={this.handleKeyUp.bind(this)} name="name" ref="stringValue" rows="8" cols="40" placeholder="What's this card about?"></textarea>
-        <button onClick={this.props.onStayButtonPush} className="btn">Save and Add Another</button>
-        <button onClick={this.props.onButtonPush} className="btn">Save and View Dashboard</button>
-=======
-        <input
-          className="btn btn-primary submit"
-          onKeyUp={this.handleTitleKeyUp.bind(this)}
-          ref="titleValue"
-          type="text" placeholder="Title"
-        />
->>>>>>> f6a25cb2fb946c6c686497d92efc934a3688994e
-
         <div className="input-info">
           <input
             className="input-title"
-            type="text" name="title"
-            placeholder="Title" value=""
+            onKeyUp={this.handleTitleKeyUp.bind(this)}
+            ref="titleValue"
+            type="text" placeholder="Title"
           />
-
           <textarea
             className="input-note"
             onKeyUp={this.handleKeyUp.bind(this)}
