229d9dc43eeff4a89dd984e64fd953bcf89daa49
Namanda Musoke <nvmusoke@gmail.com>
2017-01-18 16:13:35 -0600

    added classnames to navlink & css to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,10 @@ class Header extends Component {
   render() {
     return (
       <nav>
-      <ul>
-        <li><NavLink className="navlink" to="/dashboard" onlyActiveOnIndex>Dashboard</NavLink></li>
-        <li><NavLink className="navlink" to="/lookup" onlyActiveOnIndex>Lookup</NavLink></li>
-        <li><NavLink className="navlink" to="/about" onlyActiveOnIndex>About</NavLink></li>
+      <ul className="nav-ul">
+        <li className="nav-li"><NavLink className="navlink" to="/dashboard" onlyActiveOnIndex>Dashboard</NavLink></li>
+        <li className="nav-li"><NavLink className="navlink" to="/lookup" onlyActiveOnIndex>Lookup</NavLink></li>
+        <li className="nav-li"><NavLink className="navlink" to="/about" onlyActiveOnIndex>About</NavLink></li>
 
       </ul>
     </nav>
