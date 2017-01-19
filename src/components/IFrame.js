d1f0d32d06a8a6d84875b3c83a5d20e2b7a93e6c
christopher barragan <barraganchris95@gmail.com>
2017-01-19 11:05:27 -0600

    small changes in IFrame and App.css

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import iframe from 'react-iframe';
 
 class iFrame extends Component {
@@ -7,7 +8,7 @@ class iFrame extends Component {
     return (
 
       <div className="iframe">
-        <iframe src="http://www.w3schools.com/embed" height="700px" width="800px">
+        <iframe src="http://www.w3schools.com/" height="700px" width="800px">
       </iframe>
       </div>
     )
