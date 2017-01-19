247121a87eeee7724a4135fe4cff2f1bca480496
christopher barragan <barraganchris95@gmail.com>
2017-01-19 09:23:35 -0600

    small commits

--- src/components/Lookup.js
+++ src/components/Lookup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { hashHistory } from 'react-router';
-import { firebase } from '../utils/firebase';
+// import { hashHistory } from 'react-router';
+// import { firebase } from '../utils/firebase';
 
 import iFrame from './IFrame';
 import CardProcess from './CardProcess';
