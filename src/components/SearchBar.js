5f8e6193d05317d204cbee741a8429625dace5a7
Josh Foster <josh@allenb.com>
2017-01-19 10:59:48 -0600

    fixed one input field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { firebase, firebaseListToArray } from 'firebase';
 
 class SearchBar extends Component {
 
   render() {
     return (
       <div className="search-dashboard">
-        <input className="search" type="text" placeholder="Search"></input>
+        <input onKeyUp={this.props.onSearch} className="search" type="text" placeholder="Search"></input>
       </div>
     );
   }
